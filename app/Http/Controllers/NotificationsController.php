3caa92fcffae84b04812a57967ccfc2c82e881f7
Nafies Luthfi <nafiesl@gmail.com>
2017-06-22 15:29:45 +0800

    Update user's api_token attibute after logged in

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
